87494bd6aae9aef570383f131ef7ba64a755a8de
aleksandrasobieszek <72602011+aleksandrasobieszek@users.noreply.github.com>
2020-10-23 11:11:20 +0200

    Pierwsze zajęcia
    Pierwszy push

---
+++ Godot01/Node.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var sayHello = "Hello hello"
+var firstNumber = 10
+var secondNumber = 2345
+
+func _ready():
+#	print("Hello world")
+#	print(2+7)
+	print(sayHello)
+	print(firstNumber + secondNumber)
+	
+#func _process(delta):
+#	print("Hello world")
